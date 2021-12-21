56df41830edb0960b2c0740ac6a40cb5d49caee6
Sylwia Ziemińska <sylwia.zieminskaa@gmail.com>
2021-12-21 16:30:31 +0100

    Corrections

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import {settings, select, classNames} from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
@@ -14,23 +14,22 @@ const app = {
 
     let pageMatchingHash = thisApp.pages[0].id;
 
-    for(let page of thisApp.pages){
-      if(page.id === idFromHash){
+    for (let page of thisApp.pages) {
+      if (page.id === idFromHash) {
         pageMatchingHash = page.id;
         break;
       }
     }
     thisApp.activatePage(pageMatchingHash);
 
-
-    for(let link of thisApp.navLinks){
-      link.addEventListener('click', function(event){
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
         const clickedElement = this;
         event.preventDefault();
 
         /* get page id from href attribute */
         const id = clickedElement.getAttribute('href').replace('#', '');
-        
+
         /* run thisApp.activatePage with that id */
         thisApp.activatePage(id);
 
@@ -54,9 +53,9 @@ const app = {
       page.classList.toggle(classNames.pages.active, page.id === pageId);
     }
     /* add class 'active to matching links, remove from non-matching */
-    for(let link of thisApp.navLinks){
+    for (let link of thisApp.navLinks) {
       link.classList.toggle(
-        classNames.nav.active, 
+        classNames.nav.active,
         link.getAttribute('href') === '#' + pageId
       );
     }
@@ -110,7 +109,7 @@ const app = {
     });
   },
 
-  initBooking: function (){
+  initBooking: function () {
     const thisApp = this;
 
     thisApp.bookWidget = document.querySelector(select.containerOf.booking);
--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,24 +1,23 @@
-import { render } from 'node-sass';
+import {templates} from '../settings.js';
 
 class Booking {
   constructor(element) {
     const thisBooking = this;
-    
-    render(element);
-    initWidgets();
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
   }
 
-  render(element){
-      const thisBooking = this;
+  render(element) {
+    const thisBooking = this;
 
-      const generatedHTML = templates.bookingWidget,
+    const generatedHTML = templates.bookingWidget();
 
-      thisBooking.dom = {};
+    thisBooking.dom = {}, 
+    thisBooking.dom.wrapper = element;
 
-      thisBooking.dom.wrapper = element;
-    
-      thisBooking.dom.wrapper.innerHTML(generatedHTML);
-
-
+    thisBooking.dom.wrapper.innerHTML(generatedHTML);
   }
 }
+
+export default Booking;
